c8dd5c1af58a74fb550d517fca26051bcbfb81fd
Shadman Shaikh <shadmanforreal45@gmail.com>
2025-01-11 11:47:02 +0400

    added ways of initializing strings

---
+++ vars-string-nums.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+//global declaration 
+var xan string = "hello , xan"
+
+func main(){
+	// ways of declaring & initializing a string
+	var x string = "chadmax"
+	var y string = "gigachad"
+	z := "gomax"
+	fmt.Println(x , y  , z , xan) 
+}
